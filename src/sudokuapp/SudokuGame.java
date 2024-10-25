690a411cc21c66502d0ff5d2caffd39904ef2b04
Jorge Aguirre <134814145+NibsNibsXD@users.noreply.github.com>
2024-10-25 15:14:39 -0600

    correcion

--- src/sudokuapp/SudokuGame.java
+++ src/sudokuapp/SudokuGame.java
@@ -2,40 +2,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package SudokuApp;
+package sudokuapp;
 
 /**
  *
  * @author Jorge Aguirre
  */
-
-
 public class SudokuGame extends SudokuGrid implements SudokuValidator {
-
     private int[][] grid; 
     
     public SudokuGame() {
         grid = new int[9][9]; 
     }
     
-    @Override
     public void initializeGrid() {
         grid[0][0] = 5; grid[0][1] = 3; grid[0][4] = 7;
         grid[1][0] = 6; grid[1][3] = 1; grid[1][4] = 9;
         grid[1][5] = 5; grid[2][1] = 9; grid[2][2] = 8;
         grid[2][7] = 6;
     }
-
+    
+    
     @Override
     public void setCell(int row, int col, int value) {
+    
         if (row >= 0 && row < 9 && col >= 0 && col < 9 && value >= 1 && value <= 9) {
             grid[row][col] = value;
         } else {
             System.out.println("no valido");
         }
     }
-
-    @Override
+    
+    
     public int getCell(int row, int col) {
         if (row >= 0 && row < 9 && col >= 0 && col < 9) {
             return grid[row][col];
@@ -44,8 +42,7 @@ public class SudokuGame extends SudokuGrid implements SudokuValidator {
             return -1;
         }
     }
-
-    @Override
+    
     public boolean isValidMove(int row, int col, int value) {
         if (row < 0 || row >= 9 || col < 0 || col >= 9 || value < 1 || value > 9) {
             System.out.println("Posición o valor inválido.");
@@ -79,7 +76,9 @@ public class SudokuGame extends SudokuGrid implements SudokuValidator {
 
         return true;
     }
-
+    
+    
+    
     public boolean isBoardComplete() {
     for (int row = 0; row < 9; row++) {
         for (int col = 0; col < 9; col++) {
@@ -95,7 +94,7 @@ public class SudokuGame extends SudokuGrid implements SudokuValidator {
             grid[row][col] = value;
         }
     }
-    // si nada retorna sera true
+    // Si todo retorna es true
     return true;
 }
 
